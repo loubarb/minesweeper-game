292cbe71390ea15700d6eb156afcd7333a211c82
loubarb <louis.barbati@gmail.com>
2022-06-29 00:18:44 -0400

    Added function for marking tiles with right-click and updating mine count based on tiles marked

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,7 @@
 /* -----------------------------------LOGIC----------------------------- */
 
 // object of tile status classes from css
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
   HIDDEN: 'hidden',
   MINE: 'mine',
   NUMBER: 'number',
@@ -10,16 +10,25 @@ const TILE_STATUSES = {
 
 export function createBoard(boardSize, numberOfMines) {
   const board = []
+  const minePositions = getMinePositions(boardSize, numberOfMines)
+
   for (let x = 0; x < boardSize; x++) {
     const row = []
     for (let y = 0; y < boardSize; y++) {
       const element = document.createElement('div')
       element.dataset.status = TILE_STATUSES.HIDDEN
-      
+
       const tile = {
         element,
         x,
-        y
+        y,
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
+        get status() {
+          return this.element.dataset.status
+        },
+        set status(value) {
+          this.element.dataset.status = value
+        },
       }
 
       row.push(tile)
@@ -27,4 +36,42 @@ export function createBoard(boardSize, numberOfMines) {
     board.push(row)
   }
   return board
+}
+
+export function markTile(tile) {
+  if (tile.status !== TILE_STATUSES.HIDDEN && tile.status !== TILE_STATUSES.MARKED) {
+    return
+  }
+
+  if (tile.status === TILE_STATUSES.MARKED) {
+    tile.status = TILE_STATUSES.HIDDEN
+  } else {
+    tile.status = TILE_STATUSES.MARKED
+  }
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = []
+
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize)
+    }
+
+    if (!positions.some(p => positionMatch(p, position))) {
+      positions.push(position)
+    }
+  }
+
+  return positions
+}
+
+// checks that there are no repeats for overlapping mines
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y
+} 
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size)
 }
--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 /* -----------------------------------UI----------------------------- */
-import { createBoard } from "./minesweeper.js"
+import { TILE_STATUSES, createBoard, markTile } from "./minesweeper.js"
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 2
+const NUMBER_OF_MINES = 10
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector('.board')
+const minesLeft = document.querySelector('[data-mine-count]')
 
 board.forEach(row => {
   row.forEach(tile => {
     boardElement.append(tile.element)
+    tile.element.addEventListener('click', (e) => {
+      console.log(e.currentTarget)
+
+    })
+    // handle right-clicking with contextmenu
+    tile.element.addEventListener('contextmenu', (e) => {
+      e.preventDefault()
+      markTile(tile)
+      listMinesLeft()
+    })
   })
 })
 
 // fills in board --size variable from board class in css
 boardElement.style.setProperty('--size', BOARD_SIZE)
+
+minesLeft.textContent = NUMBER_OF_MINES
+
+// updates mine count when marking tiles
+function listMinesLeft() {
+  const markedTilesCount = board.reduce((count, row) => {
+    return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+  }, 0)
+
+  minesLeft.textContent = NUMBER_OF_MINES - markedTilesCount
+}
