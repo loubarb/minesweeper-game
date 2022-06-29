74746788e8ebd4d651a46081e5437a9f87661a7d
loubarb <louis.barbati@gmail.com>
2022-06-28 23:18:30 -0400

    Added JS files for UI and logic and populated board

---
+++ script.js
@@ -0,0 +1,17 @@
+/* -----------------------------------UI----------------------------- */
+import { createBoard } from "./minesweeper.js"
+
+const BOARD_SIZE = 10
+const NUMBER_OF_MINES = 2
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
+const boardElement = document.querySelector('.board')
+
+board.forEach(row => {
+  row.forEach(tile => {
+    boardElement.append(tile.element)
+  })
+})
+
+// fills in board --size variable from board class in css
+boardElement.style.setProperty('--size', BOARD_SIZE)
